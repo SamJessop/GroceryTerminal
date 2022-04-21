eec1f4a59da012fdf1e584578a7b8fb93754e046
SamJessop <samuel.jessop@gmail.com>
2022-04-21 14:18:26 +1200

    Initial commit

---
+++ PointOfSalesTerminal.cs
@@ -0,0 +1,17 @@
+﻿using System;
+
+namespace GroceryTerminal
+{
+    public class PointOfSalesTerminal
+    {
+        public void ScanProduct(string Product)
+        {
+            
+        }
+
+        public decimal CalculatePrice()
+        {
+            return 6M;
+        }
+    }
+}
