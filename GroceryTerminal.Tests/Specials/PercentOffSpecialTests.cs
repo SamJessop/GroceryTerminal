86157c100dfe0a62bc953dc317b24ea97c392592
SamJessop <samuel.jessop@gmail.com>
2022-04-22 13:04:52 +1200

    Added percent off special

---
+++ GroceryTerminal.Tests/Specials/PercentOffSpecialTests.cs
@@ -0,0 +1,26 @@
+﻿using FluentAssertions;
+using GroceryTerminal.Specials;
+using NUnit.Framework;
+
+namespace GroceryTerminal.Tests.Specials
+{
+    [TestFixture]
+    public class PercentOffSpecialTests
+    {
+        [TestCase(10, 1, 50, 5)]
+        [TestCase(10, 1, 30, 7)]
+        [TestCase(10, 2, 30, 14)]
+        public void CalculatePrice_calculates_correct_price(decimal productPrice, int timesScanned, int percentOff, decimal expectedPrice)
+        {
+            var testClass = new PercentOffSpecial { PercentOff = percentOff };
+
+            var scannedProduct = new ScannedProduct { Product = new Product("A", productPrice, testClass), TimesScanned = timesScanned };
+
+            // Act
+            var result = testClass.CalculatePrice(scannedProduct);
+
+            // Assert
+            result.Should().Be(expectedPrice);
+        }
+    }
+}
---
+++ GroceryTerminal/Specials/PercentOffSpecial.cs
@@ -0,0 +1,14 @@
+﻿namespace GroceryTerminal.Specials
+{
+    public class PercentOffSpecial : ISpecial
+    {
+        public int PercentOff { get; set; }
+
+        public decimal CalculatePrice(ScannedProduct scannedProduct)
+        {
+            var productPrice = scannedProduct.Product.Price / 100 * (100 - PercentOff);
+
+            return productPrice * scannedProduct.TimesScanned;
+        }
+    }
+}
